73b674f04e830ffdc424981a982bd15583d6f472
yanis <yanis.lepors@gmail.com>
2022-11-16 16:51:02 +0100

    n

--- script.js
+++ script.js
@@ -1,30 +1,102 @@
+var canvas = document.getElementById('zone');
+var ctx = canvas.getContext('2d');
+
+var largeur=hauteur=20;
+var x= Math.trunc(canvas.width/2);
+var y= Math.trunc(canvas.height/2);
 
 
+var pommeX = Math.trunc(Math.random() * canvas.width/largeur)*largeur;
+var pommeY = Math.trunc(Math.random() * canvas.height/hauteur)*hauteur;
 
+var pommeRadius = 10;
 
+var depX=depY=0
+var trace = []
+var tailleTrace = 50
+var score = 0;
+
+var scoreH2 = document.getElementById('score');
+scoreH2.innerText = ("Votre score : "+score);
+
+var hist = 0
+
+var intervalID = setInterval(game,10);
 document.addEventListener("keydown", keyboard);
 
 
 function game() {
+    y+=depY
+    x+=depX
 
+    trace.push({x:x,y:y});
+
+    while(trace.length>tailleTrace){
+        trace.shift();
+    }
+    ctx.fillStyle="#f1c40f";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(x, y, largeur, hauteur);
+    for(var i=0;i<trace.length;i++) {
+        ctx.fillRect(trace[i].x,trace[i].y, largeur, hauteur);
+    }
+
+    ctx.beginPath();
+    ctx.arc(pommeX+pommeRadius, pommeY+pommeRadius, pommeRadius, 0, 2 * Math.PI);
+    ctx.fillStyle = "#e74c3c";
+    ctx.fill();
+    ctx.closePath();
+
+
+    if(x==pommeX && y ==pommeY){
+        tailleTrace = tailleTrace + 10
+        
+        pommeX = Math.trunc(Math.random() * canvas.width/largeur)*largeur;
+        pommeY = Math.trunc(Math.random() * canvas.height/hauteur)*hauteur;
+
+        score = score + 1
+        scoreH2.innerText = ("Votre score : "+score);
+    }
+
+
+    
 }
 
 
 function keyboard(evt) {
-
-    
     switch (evt.keyCode) {
     case 37:
-        console.log("left");
+
+        if (hist == 39) break;
+        depY = 0
+        depX = -1
+        hist = 37
         break;
     case 38:
-        console.log("up");
+
+        if (hist == 40) break;
+        depX = 0
+        depY = -1
+        hist = 38
         break;
     case 39:
-        console.log("right");
+
+        if (hist == 37) break;
+        depX = 1
+        depY = 0
+        hist = 39
         break;
     case 40:
-        console.log("down");
+
+        if (hist == 38) break;
+        depX = 0
+        depY = 1
+        hist = 40
+        break;
+    case 32:
+        console.log("space");
+        depX = 0
+        depY = 0
         break;
     default:
         console.log("other");
